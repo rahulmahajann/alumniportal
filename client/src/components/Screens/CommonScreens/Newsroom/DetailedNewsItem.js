a5b7a2ceeed35030ae250090e922a7906c2dff1f
rahulmahajann <rahul@markowate.com>
2022-03-22 17:50:23 +0530

    data added on news detail page

--- client/src/components/Screens/CommonScreens/Newsroom/DetailedNewsItem.js
+++ client/src/components/Screens/CommonScreens/Newsroom/DetailedNewsItem.js
@@ -3,7 +3,12 @@ import React from "react";
 function DetailedNewsItem(props){
     const {newsDetail} = props;
     return(
-        <h1>this is detailed news item page</h1>
+        // <h1>this is detailed news item page</h1>
+        <>
+            <img src = {newsDetail.img} alt = 'news Image' />
+            <p>CreatedAt: {newsDetail.createdAt.slice(0,10)}</p>
+            <p>{newsDetail.content}</p>
+        </>
     )
 }
 
